a72d1e6c084daee8a2355e2151d5a6dfdd4799bc
Brian Hopkins <brianhh1230@gmail.com>
2023-04-24 07:49:42 -0400

    feat(init): initial files

---
+++ fantasy_mlb.py
@@ -0,0 +1,60 @@
+from pybaseball import standings
+import pandas as pd
+
+
+draft_picks_list = []
+
+with open('draft_picks.txt', 'r') as file:
+    for line in file:
+        player_name, teams = line.strip().split(': ')
+        player_teams = teams.split(',')
+        while len(player_teams) < 6:
+            player_teams.append('')
+        draft_picks_list.append([player_name] + player_teams)
+
+draft_picks_df = pd.DataFrame(draft_picks_list, columns=['Name', 'Pick 1', 'Pick 2', 'Pick 3', 'Pick 4', 'Pick 5', 'Pick 6'])
+
+# Define the teams that each player is associated with
+player_teams = {player[0]: player[1:] for player in draft_picks_list}
+
+
+# Get current MLB standings
+mlb_standings = standings()
+
+# Prepare Standings DataFrame
+standings_df = pd.concat(mlb_standings)
+
+# Calculate the wins and losses for each player
+player_data = []
+for player_name, teams in player_teams.items():
+    player_wins = 0
+    player_losses = 0
+    for team in teams:
+        team_wins = 0
+        team_losses = 0
+        for df in mlb_standings:
+            matching_rows = df[df['Tm'] == team]
+            if not matching_rows.index.empty:
+                team_record = matching_rows.iloc[0]
+                team_wins = int(team_record['W'])
+                team_losses = int(team_record['L'])
+
+        player_wins += team_wins
+        player_losses += team_losses
+    if player_wins + player_losses == 0:
+        player_win_pct = 0
+    else:
+        player_win_pct = player_wins / (player_wins + player_losses)
+    player_data.append({'Player Name': player_name, 'Wins': player_wins, 'Losses': player_losses, 'Win Percentage': player_win_pct})
+
+# Prepare Player Standings DataFrame
+player_standings_df = pd.DataFrame(player_data)
+player_standings_df.sort_values(by='Win Percentage', ascending=False, inplace=True)
+
+# Write DataFrames to Excel
+with pd.ExcelWriter('fantasy_mlb_results.xlsx', engine='openpyxl') as writer:
+    draft_picks_df.to_excel(writer, sheet_name='Draft Picks', index=False)
+    standings_df.to_excel(writer, sheet_name='MLB Standings', index=False)
+    player_standings_df.to_excel(writer, sheet_name='Player Standings', index=False)
+
+print("Data successfully exported to fantasy_mlb_results.xlsx")
